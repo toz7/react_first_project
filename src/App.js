e84eb0cf0ee8a04233f44194e1c1f5276e079645
toz7 <48638110+toz7@users.noreply.github.com>
2022-11-09 09:55:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Nav from './components/Nav';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Nav />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer /> 
       </header>
+      
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="copyright">
+            <p>Created by me using React.</p>
+        </div>
+    )
+}
+
+export default Footer;
